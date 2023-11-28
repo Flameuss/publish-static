5803ecdf946587549933de5ebfbbf538e8769ec1
flameuss <luis.henrique_campos@outlook.com.br>
2023-11-28 18:02:19 -0300

    Updates

---
+++ precache-manifest.2a962d2cb2de922177c57ffd13561552.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086c1655ec9ce125fe17c596bdcf6794",
+    "url": "/nature-based-solutions/index.html"
+  },
+  {
+    "revision": "0e69677eb8321366663e",
+    "url": "/nature-based-solutions/static/css/main.9e8d31ff.chunk.css"
+  },
+  {
+    "revision": "3342370f5dd606fa27a3",
+    "url": "/nature-based-solutions/static/js/2.39606e46.chunk.js"
+  },
+  {
+    "revision": "6211132799115a7b047d842c9563abc0",
+    "url": "/nature-based-solutions/static/js/2.39606e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e69677eb8321366663e",
+    "url": "/nature-based-solutions/static/js/main.8397db25.chunk.js"
+  },
+  {
+    "revision": "e6a79a80e95cd7d7e33e",
+    "url": "/nature-based-solutions/static/js/runtime-main.feeee8b9.js"
+  },
+  {
+    "revision": "92a0e7873d8e3848c90db8cb23aaa5f5",
+    "url": "/nature-based-solutions/static/media/agile-logo.92a0e787.png"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/nature-based-solutions/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "1286f05c52e656633da73cad591039c6",
+    "url": "/nature-based-solutions/static/media/inpe-logo.1286f05c.png"
+  },
+  {
+    "revision": "60185f7baccf79db3e8e58abd548fa51",
+    "url": "/nature-based-solutions/static/media/nature-logo.60185f7b.png"
+  },
+  {
+    "revision": "fac979ae8da806768348ec2eb2d8e36a",
+    "url": "/nature-based-solutions/static/media/netzero-logo.fac979ae.png"
+  },
+  {
+    "revision": "8bddce908c10fafefe24f9310594a363",
+    "url": "/nature-based-solutions/static/media/oxford-logo.8bddce90.png"
+  },
+  {
+    "revision": "360a3a1ba889bb3602a528589999bc10",
+    "url": "/nature-based-solutions/static/media/resume-en.360a3a1b.png"
+  },
+  {
+    "revision": "900a753361381592ae08fb4274790a85",
+    "url": "/nature-based-solutions/static/media/resume-pt.900a7533.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/publish-static/precache-manifest.1cea9fe3268245d377ded3958d7528a2.js"
+  "/nature-based-solutions/precache-manifest.2a962d2cb2de922177c57ffd13561552.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/publish-static/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nature-based-solutions/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
