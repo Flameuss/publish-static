1621caaa8d35ba9c884e7712e9dd220ce91df71e
flameuss <luis.henrique_campos@outlook.com.br>
2023-11-27 21:30:02 -0300

    Updates

---
+++ precache-manifest.4851d1cf694cf92d47e84395344c8e97.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2af4fa2ab680884bfc97cc9c21ef165",
+    "url": "/publish-static/index.html"
+  },
+  {
+    "revision": "ad57fc270df53521adce",
+    "url": "/publish-static/static/css/main.9e8d31ff.chunk.css"
+  },
+  {
+    "revision": "f78c43731b5f18517f61",
+    "url": "/publish-static/static/js/2.bf7f671b.chunk.js"
+  },
+  {
+    "revision": "6211132799115a7b047d842c9563abc0",
+    "url": "/publish-static/static/js/2.bf7f671b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad57fc270df53521adce",
+    "url": "/publish-static/static/js/main.916abf26.chunk.js"
+  },
+  {
+    "revision": "5988713cd20b2c311760",
+    "url": "/publish-static/static/js/runtime-main.90bd9fb8.js"
+  },
+  {
+    "revision": "92a0e7873d8e3848c90db8cb23aaa5f5",
+    "url": "/publish-static/static/media/agile-logo.92a0e787.png"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/publish-static/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "1286f05c52e656633da73cad591039c6",
+    "url": "/publish-static/static/media/inpe-logo.1286f05c.png"
+  },
+  {
+    "revision": "60185f7baccf79db3e8e58abd548fa51",
+    "url": "/publish-static/static/media/nature-logo.60185f7b.png"
+  },
+  {
+    "revision": "fac979ae8da806768348ec2eb2d8e36a",
+    "url": "/publish-static/static/media/netzero-logo.fac979ae.png"
+  },
+  {
+    "revision": "8bddce908c10fafefe24f9310594a363",
+    "url": "/publish-static/static/media/oxford-logo.8bddce90.png"
+  },
+  {
+    "revision": "360a3a1ba889bb3602a528589999bc10",
+    "url": "/publish-static/static/media/resume-en.360a3a1b.png"
+  },
+  {
+    "revision": "900a753361381592ae08fb4274790a85",
+    "url": "/publish-static/static/media/resume-pt.900a7533.png"
+  }
+]);
